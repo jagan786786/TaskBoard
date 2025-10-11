72a1af623314564f7fd04b3369ee9ee50df25184
Jagannath Patro <136817866+jagan786786@users.noreply.github.com>
2025-10-11 18:36:48 +0530

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 function connectToDb() {
     mongoose.connect(process.env.DB_CONNECT
     ).then(() => {
+        //sfbshjfbsfdskfdsfkdsnfkdsnfkjdsnfks
         console.log('Connected to DB');
     }).catch(err => console.log(err));
 }
