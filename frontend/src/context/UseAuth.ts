2f6f4ff1005507f421ac487540866e9bd1eaad08
Jagannath Patro <136817866+jagan786786@users.noreply.github.com>
2025-10-12 13:54:10 +0530

    Update UseAuth.ts

--- frontend/src/context/UseAuth.ts
+++ frontend/src/context/UseAuth.ts
@@ -3,6 +3,9 @@ import { AuthContext } from './auth-context';
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
+  //fhgfjgytdyftydhgjkl;jhgfdsdrftgyjhukjl
+  //erdtfyguijkopokiuytfdrertfyguhij
+  console.log(context);
   if (!context) throw new Error('useAuth must be used within an AuthProvider');
   return context;
 };
