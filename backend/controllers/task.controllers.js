1b6291e08876737930cfbed3a731fcaa1e503102
Jagannath Patro <136817866+jagan786786@users.noreply.github.com>
2025-10-13 02:28:11 +0530

    Update task.controllers.js

--- backend/controllers/task.controllers.js
+++ backend/controllers/task.controllers.js
@@ -35,6 +35,9 @@ exports.createTask = async (req, res, next) => {
       "email"
     );
 
+    //hfghejgberjgeryuijdfskfghfrjds
+    console.log(populated);
+
     res.status(201).json(normalizeTask(populated));
   } catch (err) {
     next(err);
