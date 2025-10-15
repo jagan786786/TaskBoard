a801b0c357440db9ed46c64185b3c65358d0f745
Jagannath Patro <136817866+jagan786786@users.noreply.github.com>
2025-10-15 07:46:34 +0530

    Update comment.controllers.js

--- backend/controllers/comment.controllers.js
+++ backend/controllers/comment.controllers.js
@@ -19,8 +19,6 @@ exports.addComment = async (req, res, next) => {
     //Log to the comment to know better
     comment = await comment.populate("authorId", "email");
 
-    //hfjskfskjfhsfkjshfks
-    console.log(comment);
     // flatten author
     res.status(201).json({
       id: comment._id,
@@ -39,9 +37,6 @@ exports.listComments = async (req, res, next) => {
     const comments = await Comment.find({ taskId })
       .populate("authorId", "email")
       .sort({ createdAt: 1 });
-
-    //shfjshfsfhskjfhsjk
-    console.log(comments);
 
     res.json(
       comments.map((c) => ({
