38063d88ef63e7170b45fe26b4fa213025990070
Jagannath Patro <136817866+jagan786786@users.noreply.github.com>
2025-10-23 14:20:46 +0530

    Add console log after creating a comment
    Added console log for debugging comment creation.

--- backend/controllers/comment.controllers.js
+++ backend/controllers/comment.controllers.js
@@ -18,6 +18,9 @@ exports.addComment = async (req, res, next) => {
 
     //Log to the comment to know better
     comment = await comment.populate("authorId", "email");
+
+    //shgsdhjfgdsj
+    console.log(comment);
     // flatten author
     res.status(201).json({
       id: comment._id,
